6aa7e6ba4fe4826410453ae2f94cb3f142d7c234
nautatva <nnautatva@gmail.com>
2018-08-03 18:37:11 +0530

    Home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentComponent } from './department/department.component';
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [{ 
-  path: 'departments',  component: DepartmentComponent }];
+const routes: Routes = [
+  { path: 'departments',  component: DepartmentComponent },
+  { path: '',  component: HomeComponent }];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
