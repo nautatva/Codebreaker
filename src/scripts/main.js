eb17953f8492a0b827d63b9e9bf93b26a75d4051
ShubhamKaudewar <kaudewar@gmail.com>
2018-08-21 22:08:53 +0530

    Worked on department page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
-<script type="text/javascript">
     $(document).ready(function(){
         $('.hamburger').click(function(){
             $('nav ul').toogleClass('active');
         })
     })
-</script>
