6764c50cfddfa302a339a99625ccb77fdf2dc84c
ShubhamKaudewar <kaudewar@gmail.com>
2018-08-21 19:35:13 +0530

    script file added to src\script\main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('.hamburger').click(function(){
+            $('nav ul').toogleClass('active');
+        })
+    })
+</script>
