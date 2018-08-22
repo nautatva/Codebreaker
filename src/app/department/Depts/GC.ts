900b551464c56421ea53ea9313d7190c70543f84
atharvkotwal <atharvkotwal@rediffmail.com>
2018-08-22 16:47:10 +0530

    Minor changes CSS related

--- src/app/department/Depts/GC.ts
+++ src/app/department/Depts/GC.ts
@@ -4,9 +4,9 @@ const myteam:Team[]=[
     {name:"Atharv"}
 ]
 const mylinks:Imglink[]=[
-    {linkname:"Link 1",linkhref:"#",linkimg:"../../assets/ganeshaidol.jpg"},
-    {linkname:"google",linkhref:"https://www.google.co.in/",linkimg:"../../assets/verdade.jpg"},
-    {linkname:"Link 3",linkhref:"#",linkimg:"../../assets/VFP/VFP.png"},
+    {linkname:"Ganesh Idol",linkhref:"#",linkimg:"../../assets/ganeshaidol.jpg"},
+    {linkname:"Google",linkhref:"https://www.google.co.in/",linkimg:"../../assets/verdade.jpg"},
+    {linkname:"Voice For Purpose",linkhref:"#",linkimg:"../../assets/VFP/VFP.png"},
 ]
 export const GC:Dept = {
     deptname : "Green Campus",
