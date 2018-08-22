740aef340b919d06f4bd517ef2ff4b147f9cd758
nautatva <nnautatva@gmail.com>
2018-08-22 17:17:42 +0530

    relative paths fixed for prod

--- src/app/department/Depts/EO.ts
+++ src/app/department/Depts/EO.ts
@@ -4,14 +4,14 @@ const myteam:Team[]=[
     {name:"ABCDEFGH "}
 ]
 const mylinks:Imglink[]=[
-    {linkname:"YouTube",linkhref:"https://www.youtube.com/",linkimg:"../../assets/ganeshaidol.jpg"},
-    {linkname:"Link 5",linkhref:"#",linkimg:"../../assets/VFP/VFP.png"},
-    {linkname:"Link 6",linkhref:"#",linkimg:"../../assets/verdade.jpg"},
+    {linkname:"YouTube",linkhref:"https://www.youtube.com/",linkimg:"assets/ganeshaidol.jpg"},
+    {linkname:"Link 5",linkhref:"#",linkimg:"assets/VFP/VFP.png"},
+    {linkname:"Link 6",linkhref:"#",linkimg:"assets/verdade.jpg"},
 ]
 export const EO:Dept = {
     deptname : "Educational Outreach",
     team:myteam,
     imglinks:mylinks,
     description:"We Teach",
-    img:"../../../assets/ganeshaidol.jpg"
+    img:"../assets/ganeshaidol.jpg"
 }
--- src/app/department/Depts/GC.ts
+++ src/app/department/Depts/GC.ts
@@ -4,14 +4,14 @@ const myteam:Team[]=[
     {name:"Atharv"}
 ]
 const mylinks:Imglink[]=[
-    {linkname:"Ganesh Idol",linkhref:"#",linkimg:"../../assets/ganeshaidol.jpg"},
-    {linkname:"Google",linkhref:"https://www.google.co.in/",linkimg:"../../assets/verdade.jpg"},
-    {linkname:"Voice For Purpose",linkhref:"#",linkimg:"../../assets/VFP/VFP.png"},
+    {linkname:"Ganesh Idol",linkhref:"#",linkimg:"assets/ganeshaidol.jpg"},
+    {linkname:"Google",linkhref:"https://www.google.co.in/",linkimg:"assets/verdade.jpg"},
+    {linkname:"Voice For Purpose",linkhref:"#",linkimg:"assets/VFP/VFP.png"},
 ]
 export const GC:Dept = {
     deptname : "Green Campus",
     team:myteam,
     imglinks:mylinks,
     description:"We plant",
-    img:"../../../assets/VFP/VFP.png"
+    img:"../assets/VFP/VFP.png"
 }
