ddd0b901169fa57d1be79dc9e70e2f7f35eb28a4
nautatva <nnautatva@gmail.com>
2018-08-29 17:52:35 +0530

    responsive department for navbar and department page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,17 +13,11 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {}
 
   toggleNav() {
-    // this.overlay=true;
-    // let overlay = document.getElementById('overlay');
-    // if (overlay.style.display === 'none') {
-    //   overlay.style.display = 'absolute';
-    // } else {
-    //   overlay.style.display = 'none';
-    // }
-
-    // overlay.style.display = 'absolute';
-    // console.log(overlay.style.display);
-    console.log(this);
-    
+    let overlay = document.getElementById('overlay');
+    if (overlay.style.display === 'none') {
+      overlay.style.display = '';
+    } else {
+      overlay.style.display = 'none';
+    }
   }
 }
