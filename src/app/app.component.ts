3a3657b422e65967d805448a736f855acb04bdc5
nautatva <nnautatva@gmail.com>
2018-09-18 01:01:54 +0530

    service worker for refresh snack-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public snackBar: MatSnackBar, private swUpdate: SwUpdate) {}
   title = 'NSS, IIT Bombay';
+
+  ngOnInit() {
+    /** Check for update */
+    /* Show a prompt to update */
+    this.swUpdate.available.subscribe(event => {
+      const snackBarRef = this.snackBar.open(
+        'New version available!',
+        'Refresh',
+        {
+          duration: 60000
+        }
+      );
+
+      /* On clicking refresh */
+      snackBarRef.onAction().subscribe(() => {
+        this.swUpdate.activateUpdate().then(() => document.location.reload());
+      });
+    });
+
+    /* Check for new versions */
+    this.swUpdate.checkForUpdate();
+  }
 }
